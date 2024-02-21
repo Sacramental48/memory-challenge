dfac816f2506ff46dea82a5afd7fce9bf93e7137
Aleksey <lomberjos33hg@gmail.com>
2024-02-21 08:48:27 +0300

    added html markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="wrapper">
+        <div class="wrapper__container">
+            <button class="wrapper__start">Начать игру!</button>
+            <p>Счет: {{ rounds }}</p>
+            <div class="wrapper__field">
+                <span class="wrapper__square" :style="{'backgroundColor': getColor(index)}" v-for="(_, index) in 4" :key="index"></span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            colors: ['red', 'green', 'blue', 'yellow'],
+            rounds: 0,
+        }
+    },
+    methods: {
+        getColor(id) {
+            return this.colors[id % this.colors.length];
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
